13e3da6b6cf9a51e8d29f9c12f43f7f30f1f61c3
Mark Arrow <mark.arrow@protime.eu>
2021-05-04 16:18:24 +0200

    Renamed test file

---
+++ 2MinTicTacToeTests/BoardTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace _2MinTicTacToeTests
+{
+    public class BoardTests
+    {
+        [Fact]
+        public void Board_Has_Nine_Spots()
+        {
+
+        }
+    }
+}
